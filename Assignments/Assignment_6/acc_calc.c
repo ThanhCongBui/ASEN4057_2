d1c348c3d8a7265f2d53e5594df4aae82ced657e
tuckerEmmett <twemmett99@gmail.com>
2017-03-11 17:57:13 -0700

    Push 2

--- Assignments/Assignment_6/acc_calc.c
+++ Assignments/Assignment_6/acc_calc.c
@@ -3,6 +3,14 @@
 void accCalc(struct initSat, struct initMoon, struct outSat, struct outMoon) {
 
 printf("Unpacking structures");
+//Define initial values
+
+double mMoon=7.34767309E22;
+double mEarth=5.97219E24;
+double mSat=28833;
+double rMoon=1737100;
+double rEarth=6371000;
+
 
 // output is going to be x y vx vy 
 // unpack variables
@@ -17,6 +25,10 @@ double moonX = initMoon->x;
 double moonY = initMoon->y;
 double moonVx = initMoon->vx;
 double moonVy = initMoon->vy;
+
+double G=6.67259E-11;
+
+fSatMoon=
 
 
 return;
