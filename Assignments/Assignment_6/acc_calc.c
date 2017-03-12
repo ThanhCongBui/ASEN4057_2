d50f7e473f43cc0b3d92b0a2958d9d98f18decd3
tuckerEmmett <twemmett99@gmail.com>
2017-03-11 17:43:09 -0700

    Commit initial acc_calc

---
+++ Assignments/Assignment_6/acc_calc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void accCalc(struct initSat, struct initMoon, struct outSat, struct outMoon) {
+
+printf("Unpacking structures");
+
+// output is going to be x y vx vy 
+// unpack variables
+
+double satX = initSat->x;
+double satY = initSat->y;
+double satVx = initSat->vx;
+double satVy = initSat->vy;
+
+
+double moonX = initMoon->x;
+double moonY = initMoon->y;
+double moonVx = initMoon->vx;
+double moonVy = initMoon->vy;
+
+
+return;
+
+}
--- Assignments/Assignment_6/runCalc.sh
+++
@@ -1,7 +0,0 @@
-echo trying
-
-gcc -o calcAcc calcAcc.c
-
-./calcAcc 3 4
-
-echo tried
