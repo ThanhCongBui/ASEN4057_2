85d3b5aabb14b0a77ee65e3972ccaca1e9d04b2f
tuckerEmmett <twemmett99@gmail.com>
2017-03-12 14:37:26 -0600

    Sunday Start

--- Assignments/Assignment_6/main.c
+++ Assignments/Assignment_6/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 
-void main(){
-struct 
+void main(int argc, char *argv[]){
 
+printf("Running Program...\n");
+int test
+sscanf(argv[1],"%d",&test);
 
+printf("Input value read in as %d",test);
 return;
 
-]
+}
 
