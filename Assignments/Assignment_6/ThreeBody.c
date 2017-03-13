8e9ed394742c01f1200a59599d168311f8dd88f3
tuckerEmmett <twemmett99@gmail.com>
2017-03-13 10:50:44 -0600

    edited ThreeBody

--- Assignments/Assignment_6/ThreeBody.c
+++ Assignments/Assignment_6/ThreeBody.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "accHeader.h"
 #include "eulerHeader.h"
-
+#include <math.h>
 double ThreeBody(double argc, char *argv[] ){
 
 struct initial *sat;
 struct initial *moon;
 struct output *satOut;
 struct output *moonOut;
+
+double distEarthSat = 340000000; //meters
+double v0Sat = 1000; // meters/second
+double theta0Sat = 50; // degrees
+double distEarthMoon = 384403000; // meters
+double mMoon = 7.34767309E22; // kg
+double mEarth = 5.97219E24; // kg
+
+
 
 /*
 
@@ -18,12 +27,10 @@ Recall initial is of format:
 	double vx;
 	double vy;
 
-
 and output is of format:
 
 	double ax;
 	double ay;
-
 
 */
 
