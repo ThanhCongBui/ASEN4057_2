464cb04f6785df751932d5a1bf2b97a5d4fc6cde
tuckerEmmett <twemmett99@gmail.com>
2017-03-13 23:35:22 -0600

    Changing ThreeBody

--- Assignments/Assignment_6/ThreeBody.c
+++ Assignments/Assignment_6/ThreeBody.c
@@ -11,11 +11,18 @@ struct output *moonOut;
 
 double distEarthSat = 340000000; //meters
 double v0Sat = 1000; // meters/second
-double theta0Sat = 50; // degrees
+
 double distEarthMoon = 384403000; // meters
 double mMoon = 7.34767309E22; // kg
 double mEarth = 5.97219E24; // kg
 
+double pi = 3.14159265358979323846264338;
+
+double theta0Sat = 50/pi;
+
+
+
+sat->x = distEarthMoon*cos(theta0Sat);
 
 
 /*
