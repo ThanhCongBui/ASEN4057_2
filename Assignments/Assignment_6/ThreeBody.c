e44e24b363f446c63089d746e790b5fd13768d0a
tuckerEmmett <twemmett99@gmail.com>
2017-04-02 16:49:55 -0600

    Changed things up to correctly use main inside of ThreeBody

--- Assignments/Assignment_6/ThreeBody.c
+++ Assignments/Assignment_6/ThreeBody.c
@@ -1,49 +1,54 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include "accHeader.h"
 #include "eulerHeader.h"
 #include <math.h>
-double ThreeBody(double argc, char *argv[] ){
+#include <stdlib.h>
+#include "exits.h"
+#include "iterHeader.h"
 
-struct initial *sat;
-struct initial *moon;
-struct output *satOut;
-struct output *moonOut;
 
-double distEarthSat = 340000000; //meters
-double v0Sat = 1000; // meters/second
-
-double distEarthMoon = 384403000; // meters
-double mMoon = 7.34767309E22; // kg
-double mEarth = 5.97219E24; // kg
-
-double pi = 3.14159265358979323846264338;
-
-double theta0Sat = 50/pi;
+double main(double argc, char *argv[] ){
 
 
 
-sat->x = distEarthMoon*cos(theta0Sat);
+struct input *sat;
+sat = malloc(sizeof(struct input));
+struct input *moon;
+moon = malloc(sizeof(struct input));
 
 
-/*
-
-Recall initial is of format:
-
-	double x;
-	double y;
-	double vx;
-	double vy;
-
-and output is of format:
-
-	double ax;
-	double ay;
-
-*/
+struct initial *satAcc;
+satAcc = malloc(sizeof(struct initial));
+struct initial *moonAcc;
+moonAcc = malloc(sizeof(struct initial));
 
 
-double timestep = 0.01;
+struct inEuler *in;
+in = malloc(sizeof(struct inEuler));
+struct outEuler *out;
+out = malloc(sizeof(struct outEuler));
+// Now define all initial conditions. This might be cleaner in a separate file.
 
+
+double deltaVx = 0;
+double deltaVy = 0;
+
+sat->x = 340000000*cos(50*3.14159265/180);
+sat->y = 340000000*sin(50*3.14159265/180);
+sat->vx = 1000*cos(50*3.14159265/180) + deltaVx;
+sat->vy = 1000*sin(50*3.14159265/180) + deltaVy;
+sat->ax = 0;
+sat->ay = 0;
+
+moon->x = 384403000*cos(42.5*3.14159265/180);
+moon->y = 384403000*sin(42.5*3.14159265/180);
+moon->vx = -1012.0715844*sin(42.5*3.14159265/180); // this value is pulled from matlab code instead of dynamically calculating it
+moon->vy = 1012.0715844*cos(42.5*3.14159265/180);
+
+
+double runIterate = 0;
+double clearance = 10000;
+//runIterate = iterateSolution(sat,moon,clearance);
 
 
 
