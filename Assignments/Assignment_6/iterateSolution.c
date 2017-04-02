d0bab6a0a2aabca7b60950c581d11f5969aceee2
tuckerEmmett <twemmett99@gmail.com>
2017-04-02 13:07:05 -0600

    Headed to lunch

--- Assignments/Assignment_6/iterateSolution.c
+++ Assignments/Assignment_6/iterateSolution.c
@@ -10,7 +10,34 @@ double iterateSolution(struct input *sat, struct input *moon, double clearance){
 
 
 
+/* So this is going to look something like this :
 
+
+take in the acc_calc struct
+
+call euler like so:
+
+velocity_update = euler(acceration);
+
+position_update = euler(velocity_update);
+
+
+check exitConditions()
+
+if == 0, continue. Otherwise update all structs and return out of this function. 
+
+
+
+initial_velocity = velocity_update;
+initial_position = position_update;
+
+and run again.
+
+
+We might have to do this recursively instead of iterating for some set amount of iterates.
+I'm not too good with recursive programming but I think it'll be our best oppportunity here.
+
+*/
 
 
 
