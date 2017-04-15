a1f75117cdc0e682aefe9f70ed93a33c28b9f52e
tuckerEmmett <twemmett99@gmail.com>
2017-04-15 16:20:14 -0600

    File is working. Now onto the bash section.

--- Assignments/Assignment_7/mat_ops.c
+++ Assignments/Assignment_7/mat_ops.c
@@ -56,7 +56,7 @@ fclose(CFile);
 
 char * full_nameXX;
 char * full_nameXP;
-char * full_namePM;
+char * full_namePX;
 char * full_nameXM;
 char * full_nameMX;
 char * fillerP = "_plus_";
@@ -68,7 +68,7 @@ full_nameXX = malloc(strlen(argv[1]) + strlen(fillerX) + strlen(argv[2]) + strle
 
 full_nameXP = malloc(strlen(argv[1]) + strlen(fillerX) + strlen(argv[2]) + strlen(fillerP) + strlen(argv[3]));
 
-full_namePM = malloc(strlen(argv[1]) + strlen(fillerP) + strlen(argv[2]) + strlen(fillerM) + strlen(argv[3]));
+full_namePX = malloc(strlen(argv[1]) + strlen(fillerP) + strlen(argv[2]) + strlen(fillerM) + strlen(argv[3]));
 
 full_nameXM = malloc(strlen(argv[1]) + strlen(fillerX) + strlen(argv[2]) + strlen(fillerM) + strlen(argv[3]));
 
@@ -122,9 +122,6 @@ FILE *XXFile;
 else { printf("Dimension mismatch, cannot compute A*B*C matrix operation. \n");}
 
 
-
-printf("Proceeding into the next section of code... \n");
-
 // Second Case. A*B + C 
 if ((a_n == b_m) && (b_m == c_m) && (b_n == c_n))
 {
@@ -149,12 +146,7 @@ if ((a_n == b_m) && (b_m == c_m) && (b_n == c_n))
 			Final[ii*c_n+jj] = Mid[ii*c_n+jj] + C[ii*c_n+jj];
 				
 			}
-
-
-
-
 		}
-
 
 
 FILE *XPFile; 
@@ -172,7 +164,6 @@ FILE *XPFile;
 
 				fprintf(XPFile,"%lf ",Final[ii*c_n+jj]);
 				fprintf(XPFile,"\n");
-
 			}
 		fclose(XPFile);
 		free(Final);
@@ -181,7 +172,7 @@ FILE *XPFile;
 
 }
 
-else { printf("Dimension mismatch, cannot compute A*B*C matrix operation. \n");}
+else { printf("Dimension mismatch, cannot compute A*B + Cmatrix operation. \n");}
 
 
 
@@ -194,60 +185,161 @@ if ((b_n == c_m) && (a_m == b_m) && (c_n == a_n))
 	//Create intermediate matrices to avoid overwriting the necessary matrices
 	double * Mid;
  	double * Final;
-	Final = malloc(c_m*c_n*sizeof(double));
-	Mid=malloc(a_n*b_m*sizeof(double));
+	Final = malloc(a_m*a_n*sizeof(double));
+	Mid=malloc(b_n*a_m*sizeof(double));
+	
+
+	//First iteration to populate B*C
+	
+	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,b_m,c_m,a_m,1.0,B,b_m,C,c_m,0,Mid,a_m);
+
+	//Second iteration to populate A+Mid. Simple matrix addition iteration
+	
+	for (int ii = 0; ii<c_m; ii++){
+		for (int jj = 0; jj<c_n; jj++){
+
+			Final[ii*c_n+jj] = Mid[ii*c_n+jj] + A[ii*c_n+jj];
+				
+			}
+		}
+
+
+FILE *PXFile; 
+	strcpy(full_namePX,argv[1]);
+	strcat(full_namePX,fillerP);
+	strcat(full_namePX,argv[2]);
+	strcat(full_namePX,fillerX);
+	strcat(full_namePX,argv[3]);
+	PXFile = fopen(full_namePX,"w");
+	fprintf(PXFile,"%d %d\n",a_m,c_n);
+	
+		for (int ii = 0; ii < a_m; ii++){
+
+			for (int jj = 0; jj < c_n; jj++)
+
+				fprintf(PXFile,"%lf ",Final[ii*c_n+jj]);
+				fprintf(PXFile,"\n");
+
+			}
+		fclose(PXFile);
+		free(Final);
+		free(Mid);
+		free(full_namePX);
+
+}
+
+else { printf("Dimension mismatch, cannot compute A + B*C matrix operation. \n");}
+
+
+// Fourth Case. A*B - C
+
+if ((a_n == b_m) && (a_m == c_m) && (c_n == a_n))
+{
+	
+
+	//Create intermediate matrices to avoid overwriting the necessary matrices
+	double * Mid;
+ 	double * Final;
+	Final = malloc(a_m*a_n*sizeof(double));
+	Mid=malloc(b_n*a_m*sizeof(double));
 	
 
 	//First iteration to populate A*B
 	
 	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,a_m,b_m,c_m,1.0,A,a_m,B,b_m,0,Mid,c_m);
 
-	//Second iteration to populate Mid+C. Simple matrix addition iteration
+	//Second iteration to populate Mid-C. Simple matrix addition iteration
 	
 	for (int ii = 0; ii<c_m; ii++){
 		for (int jj = 0; jj<c_n; jj++){
 
-			Final[ii*c_n+jj] = Mid[ii*c_n+jj] + C[ii*c_n+jj];
+			Final[ii*c_n+jj] = Mid[ii*c_n+jj] - C[ii*c_n+jj];
 				
 			}
-
-
-
-
 		}
 
 
-
-FILE *XPFile; 
-	strcpy(full_nameXP,argv[1]);
-	strcat(full_nameXP,fillerX);
-	strcat(full_nameXP,argv[2]);
-	strcat(full_nameXP,fillerP);
-	strcat(full_nameXP,argv[3]);
-	XPFile = fopen(full_nameXP,"w");
-	fprintf(XPFile,"%d %d\n",a_m,c_n);
+FILE *XMFile; 
+	strcpy(full_nameXM,argv[1]);
+	strcat(full_nameXM,fillerX);
+	strcat(full_nameXM,argv[2]);
+	strcat(full_nameXM,fillerM);
+	strcat(full_nameXM,argv[3]);
+	XMFile = fopen(full_nameXM,"w");
+	fprintf(XMFile,"%d %d\n",a_m,c_n);
 	
 		for (int ii = 0; ii < a_m; ii++){
 
 			for (int jj = 0; jj < c_n; jj++)
 
-				fprintf(XPFile,"%lf ",Final[ii*c_n+jj]);
-				fprintf(XPFile,"\n");
+				fprintf(XMFile,"%lf ",Final[ii*c_n+jj]);
+				fprintf(XMFile,"\n");
 
 			}
-		fclose(XPFile);
+		fclose(XMFile);
 		free(Final);
 		free(Mid);
-		free(full_nameXP);
+		free(full_nameXM);
 
 }
 
-else { printf("Dimension mismatch, cannot compute A*B*C matrix operation. \n");}
+else { printf("Dimension mismatch, cannot compute A*B - Cmatrix operation. \n");}
 
 
 
+// Fifth Case. A-B*C
+
+if ((b_n == c_m) && (a_m == b_m) && (c_n == a_n))
+{
+	
+
+	//Create intermediate matrices to avoid overwriting the necessary matrices
+	double * Mid;
+ 	double * Final;
+	Final = malloc(a_m*a_n*sizeof(double));
+	Mid=malloc(b_m*c_n*sizeof(double));
+	
+
+	//First iteration to populate B*C
+	
+	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,b_m,c_m,a_m,1.0,B,b_m,C,c_m,0,Mid,a_m);
+
+	//Second iteration to populate A-Mid. Simple matrix addition iteration
+	
+	for (int ii = 0; ii<c_m; ii++){
+		for (int jj = 0; jj<c_n; jj++){
+
+			Final[ii*c_n+jj] = A[ii*c_n+jj] - Mid[ii*c_n+jj];
+				
+			}
+		}
 
 
+FILE *MXFile; 
+	strcpy(full_nameMX,argv[1]);
+	strcat(full_nameMX,fillerM);
+	strcat(full_nameMX,argv[2]);
+	strcat(full_nameMX,fillerX);
+	strcat(full_nameMX,argv[3]);
+	MXFile = fopen(full_nameMX,"w");
+	fprintf(MXFile,"%d %d\n",a_m,c_n);
+	
+		for (int ii = 0; ii < a_m; ii++){
+
+			for (int jj = 0; jj < c_n; jj++)
+
+				fprintf(MXFile,"%lf ",Final[ii*c_n+jj]);
+				fprintf(MXFile,"\n");
+
+			}
+		fclose(MXFile);
+		free(Final);
+		free(Mid);
+		free(full_nameMX);
+
+}
+
+else { printf("Dimension mismatch, cannot compute A - B*C matrix operation. \n");}
 //I need to do A*B*C(DONE), A*B + C (DONE), A + B*C, A*B - C, A - B*C and save each in a separate text file
 
 //For multiplication, A_n = B_m. For add/subt, A and B must be same size
