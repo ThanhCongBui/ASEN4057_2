7254a9029849d47f69673b5b956a129eb27b4260
tuckerEmmett <twemmett99@gmail.com>
2017-04-15 18:29:52 -0600

    Dinner

---
+++ Assignments/Assignment_7/HeatEqn/Build_LHS.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <cblas.h>
+#include <lapacke.h>
+
+
+int Build_LHS(){
+
+
+/*From Evans' source code: 
+
+
+K is the LHS matrix of size zeros(nodes_per_side ^2)
+
+K is filled with various conductivity values which will be computed in the conductivity file. I probably 
+should write that one first.
+
+
+
+*/
+
+
+
+return 0;
+
+
+
+}
