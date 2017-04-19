f24c7318f759b95e6c989eb5568225208f9a6c11
tuckerEmmett <twemmett99@gmail.com>
2017-04-19 13:11:27 -0600

    asdf

--- Assignments/Heat_Assignment7/main.c
+++ Assignments/Heat_Assignment7/main.c
@@ -6,23 +6,38 @@
 #include <string.h>
 
 
-void main(int argc, char * argv[]){
+void main(int argc, const char * argv[]){
 
 int cells_per_side;
 int problem_index;
+
 cells_per_side = atoi(argv[1]);
+
 problem_index = atoi(argv[2]);
 
 
 double h = 1/cells_per_side;
 int nodes_per_side = cells_per_side+1;
-
+printf("nodes value is %d \n",nodes_per_side);
 double * x_array;
 double * y_array;
 x_array = malloc(nodes_per_side*sizeof(double));
+y_array = malloc(nodes_per_side*sizeof(double));
+int *index;
+index = malloc(nodes_per_side*nodes_per_side*sizeof(int));
+
+for (int ii = 0; ii < nodes_per_side; ii++){
+
+	for (int jj = 0; jj < nodes_per_side; jj++){
+
+		index[ii+jj] =(ii)*nodes_per_side + jj;
+		
+		}
+	}
+
+printf("Value is %d\n", index[2]);
 
 
-
-return;
+return ;
 
 }
