6a0e7fefe94fb00f929549279c0f032867100104
tuckerEmmett <twemmett99@gmail.com>
2017-04-18 23:09:40 -0600

    Working on Build_LHS still

--- Assignments/Heat_Assignment7/Build_LHS.c
+++ Assignments/Heat_Assignment7/Build_LHS.c
@@ -7,12 +7,33 @@
 #include <lapacke.h>
 
 
-int Build_LHS(int cells_per_side,){
+int Build_LHS(double cells_per_side){
 
 // Build K matrix here. Need to know resolution of overall grid as well as x and y dimensions
 // Need to know Conductivity at each point, source (?) at each point, and BC at each point
 
+double h_MeshSize  = 1/cells_per_side;
+double* x_array;
+double* y_array;
+double nodes_per_side = cells_per_side + 1;
 
+//make sure that x and y arrays have the correct dimensions
+ x_array=malloc(nodes_per_side*sizeof(double));
+ y_array = malloc(nodes_per_side*sizeof(double));
+ 
+ //populate the x_array and y_array with proper "mesh" sizing. 
+for (int ii = 0; ii < nodes_per_side; ii++){
+
+  x_array[ii] = ii*h_MeshSize;
+  y_array[ii] = ii*h_MeshSize;
+  
+ }
+// Hope that the arrays are correctly sized and populated.
+
+ 
+
+
+ double * K[3][3];
 
 return 0;
 
