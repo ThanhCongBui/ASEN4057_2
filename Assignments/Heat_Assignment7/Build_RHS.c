df58115e7554edd73e70590bc073acf55d197ada
tuckerEmmett <twemmett99@gmail.com>
2017-04-19 20:03:16 -0600

    Slow going as usual. There's no way this will get completed in time. Just hope to god I pass the class

--- Assignments/Heat_Assignment7/Build_RHS.c
+++ Assignments/Heat_Assignment7/Build_RHS.c
@@ -5,18 +5,17 @@
 #include <cblas.h>
 #include <lapacke.h>
 
-int Build_RHS(double cells_per_side){
+int Build_RHS(double * F, int * index, int problem_index, int cells_per_side, double * x_array, double * y_array){
 
-  double h_MeshSize = 1/cells_per_side;
+  double h_MeshSize =/cells_per_side;
   int nodes_per_side = cells_per_side+1;
 
-  // Populate x and y arrays
-
   
+    for (int ii = 0; ii < nodes_per_side * nodes_per_side; ii++){
 
-  double *f_RHS;
-  f_RHS = malloc(nodes_per_side*sizeof(double));
-  
+		// F[ii] = BC call 
+
+		}
 
 
   return 0;
