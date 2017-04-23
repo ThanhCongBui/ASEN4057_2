9762e6b5a37a00a66541f35ec5c316ec44c67831
tuckerEmmett <twemmett99@gmail.com>
2017-04-23 17:31:38 -0600

    Source

--- Assignments/Heat_Assignment7/Build_RHS.c
+++ Assignments/Heat_Assignment7/Build_RHS.c
@@ -7,16 +7,19 @@
 #include "Build_RHS.h"
 #include "BC.h"
 
-double *Build_RHS(int nodes_per_side, double *x, double *y, int **idx, int problem_index){
+double *Build_RHS(int nodes_per_side, double *x, double *y, int *index, int problem_index){
 
-  
-int center_node;
-double *f = malloc(nodes_per_side*sizeof(double*));
+  int center;
+  double *f = malloc(nodes_per_side*sizeof(double*));
 		
+ for (int ii = 0; ii < nodes_per_side, ii++;){
+   for (int jj = 0; jj < nodes_per_side, jj++;){
 
-		
+     f[center] = Source(nodes_per_side, x[ii], y[jj], problem_index);
 
 
-  return 0;
+ }
+
+  return f;
 
 }
--- Assignments/Heat_Assignment7/Source.c
+++ Assignments/Heat_Assignment7/Source.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <cblas.h>
-#include <lapacke.h>
 #include <math.h>
 #include "Source.h"
 
@@ -21,16 +18,41 @@ double Source( int nodes_per_side, double x, double y, int problem_index){
 
 	if (problem_index == 2){
 
-	  f = 2*(y
+	  heat = 2*(y*(1-y) + x*(1-x));
 
+	}
 
+	if (problem_index == 3){
 
+	  heat = exp(-50*sqrt(pow((x-0.5),2) + pow((y-0.5),2)));
+	}
 
+	if (problem_index == 4){
 
+	  if (x < 0.1) {
+	    heat = 1;
+	  }
+	  else{
 
+	    heat = 0;
+	  }
 
+	}
+	  
+	if(problem_index == 5){
 
-return f;
+	  if( x < 0.015 && x > 0.01 && y < 0.015 && y > 0.01){
+
+	    heat = 1600000;
+	  }
+	  else{
+
+	    heat = 0;
+	  }
+
+	}
+return heat;
 
 
 }
+
