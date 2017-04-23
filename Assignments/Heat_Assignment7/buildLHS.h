5c8b9878d48ff77989059699f169849d34470a6d
tuckerEmmett <twemmett99@gmail.com>
2017-04-22 19:23:11 -0600

    Cleaning up files and improving header behavior. Symposium on Friday was fun

--- Assignments/Heat_Assignment7/buildLHS.h
+++
@@ -1,7 +0,0 @@
-#ifndef _Build_LHS_
-#define _Build_LHS_
-
-
-void Build_LHS( double * K, int * index, int problem_index, int cells_per_side, double * x_array, double * y_array);
-
-#endif
--- Assignments/Heat_Assignment7/buildRHS.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Build_RHS_
-#define _Build_RHS_
-
-void Build_RHS( double * F, int * index, int problem_index, int cells_per_side, double * x_array, double * y_array);
-
-#endif
